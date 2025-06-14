9d819992bb37c5f76703b6244002cf3550af986d
Srikanth Reddy <93464981+srikstar@users.noreply.github.com>
2025-06-14 12:29:55 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
-// const blackscreen = document.querySelector('.blackscreen');
+const blackscreen = document.querySelector('.blackscreen');
 
-// function blackscreenDisplay(){
-//     setTimeout(()=>{
-//         blackscreen.style.opacity = 0
-//         blackscreen.style.transition = 'opacity 0.3s ease-in-out'
-//     },500)
-//     setTimeout(()=>{
-//         blackscreen.style.display = 'none'
-//     },850)
-// }
+function blackscreenDisplay(){
+    setTimeout(()=>{
+        blackscreen.style.opacity = 0
+        blackscreen.style.transition = 'opacity 0.3s ease-in-out'
+    },500)
+    setTimeout(()=>{
+        blackscreen.style.display = 'none'
+    },850)
+}
 
-// blackscreenDisplay()
+blackscreenDisplay()
 
 const hamburger = document.querySelector(".hamburger-menu-container");
 const hamone = document.querySelector(".ham-one");
 const hamtwo = document.querySelector(".ham-two");
 const navdrop = document.querySelector(".navdrop");
 const navbarlogo = document.querySelector(".navbar-logo");
-const navsocials = document.querySelector(".nav-social-container");
 const linkview = document.querySelectorAll(".link-transition");
 let rotate = document.querySelector(".refresh-button-container");
 const reviewone = document.querySelector(".reviews-container-one");
@@ -63,68 +62,9 @@ qtab.forEach((tab) => {
   });
 });
 
-rotate.addEventListener("click", function () {
-  if (reviewCount % 3 == 0) {
-    reviewone.classList.remove("review-display");
-    reviewtwo.classList.add("review-display");
-  } else if (reviewCount % 3 == 1) {
-    reviewtwo.classList.remove("review-display");
-    reviewthree.classList.add("review-display");
-  } else if (reviewCount % 3 == 2) {
-    reviewthree.classList.remove("review-display");
-    reviewone.classList.add("review-display");
-  }
-  reviewCount++;
-});
-
-const classone = document.querySelector('.class-one');
-const classtwo = document.querySelector('.class-two');
-const classheadingone = document.querySelector('.course-heading-one')
-const courseparaone = document.querySelector('.course-para-one')
-const coursebtnone = document.querySelector('.course-read-more-one')
-const readmore = document.querySelector('.read-more-main-container')
-const classheadingtwo = document.querySelector('.course-heading-two')
-const courseparatwo = document.querySelector('.course-para-two')
-const coursebtntwo = document.querySelector('.course-read-more-two')
-let lastHovered = null;
-
-function handleMouseEnter(event) {
-    if (lastHovered && lastHovered !== event.target) {
-        lastHovered.classList.remove('black');
-        classheadingone.style.color = 'black';
-        courseparaone.style.color = 'black'
-        coursebtnone.style.opacity = 0;
-
-        classheadingtwo.style.color = '#cbbb99';
-        courseparatwo.style.color = '#f8f8f6'
-        coursebtntwo.style.opacity = 1;
-    }
-
-    event.target.classList.add('black');
-    if (event.target.getAttribute('data-color') === 'black') {
-        classheadingone.style.color = '#cbbb99';
-        courseparaone.style.color = '#f8f8f6'
-        coursebtnone.style.opacity = 1;
-
-        classheadingtwo.style.color = 'black';
-        courseparatwo.style.color = 'black'
-        coursebtntwo.style.opacity = 0;
-    }
-    lastHovered = event.target;
-}
-
-classone.addEventListener('mouseenter', handleMouseEnter);
-classtwo.addEventListener('mouseenter', handleMouseEnter);
-
-coursebtnone.addEventListener('click',function(){
-    readmore.style.display = 'flex'
-})
 
 hamburger.addEventListener("click", function () {
   if (activeHam) {
-    setTimeout(() => {
-      navsocials.style.opacity = 1;
-    }, 800);
     linkview.forEach((links, index) => {
       setTimeout(() => {
         links.style.transform = "translateY(0)";
@@ -135,7 +75,6 @@ hamburger.addEventListener("click", function () {
     hamone.style.transform = "rotate(45deg)";
     hamone.style.position = "relative";
     hamone.style.top = "5px";
-    hamone.style.backgroundColor = "rgba(226, 216, 190, 0.822)";
 
     navdrop.style.transform = "translateY(0vh)";
     navbarlogo.style.opacity = 0;
@@ -143,16 +82,12 @@ hamburger.addEventListener("click", function () {
     hamtwo.style.transform = "rotate(-45deg)";
     hamtwo.style.position = "relative";
     hamtwo.style.bottom = "5px";
-    hamtwo.style.backgroundColor = "rgba(226, 216, 190, 0.822)";
   } else {
     setTimeout(() => {
       linkview.forEach(function (links) {
         links.style.transform = "translateY(100px)";
       });
     }, 10);
-    setTimeout(() => {
-      navsocials.style.opacity = 0;
-    }, 100);
     setTimeout(() => {
       hamone.style.transform = "rotate(0deg)";
       hamone.style.position = "relative";
